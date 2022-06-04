d5cfc1b5e5cf7e4e094adee5a701328b667f5701
Aleksei Tarasov <aleksei.v.tarasov@gmail.com>
2022-06-04 16:49:26 +0300

    Initial commit from Aleksei Tarasov
    DaloroAT <aleksei.v.tarasov@gmail.com>

---
+++ oml/lightning/callbacks/metric.py
@@ -0,0 +1,84 @@
+from typing import Any, Optional
+
+import pytorch_lightning as pl
+from pytorch_lightning import Callback
+from pytorch_lightning.utilities.types import STEP_OUTPUT
+
+from oml.interfaces.metrics import IBasicMetric
+from oml.utils.misc import flatten_dict
+
+
+class MetricValCallback(Callback):
+    def __init__(self, metric: IBasicMetric, loader_idx: int = 0, samples_in_getitem: int = 1):
+        """
+
+        Args:
+            metric: metric
+            loader_idx: loader idx
+            samples_in_getitem: Some of the datasets return several samples when calling __getitem__,
+                so we need to handle it for the proper calculation. For most of the cases this value equals to 1,
+                but for TriDataset, which return anchor, positive and negative images, this value must be equal to 3,
+                for a dataset of pairs it must be equal to 2.
+        """
+        self.metric = metric
+        self.loader_idx = loader_idx
+        self.samples_in_getitem = samples_in_getitem
+
+        self._expected_samples = 0
+        self._collected_samples = 0
+        self._ready_to_accumulate = False
+
+    def on_validation_batch_start(
+        self, trainer: pl.Trainer, pl_module: pl.LightningModule, batch: Any, batch_idx: int, dataloader_idx: int
+    ) -> None:
+        if dataloader_idx == self.loader_idx:
+            if not self._ready_to_accumulate:
+                self._expected_samples = self.samples_in_getitem * len(trainer.val_dataloaders[dataloader_idx].dataset)
+                self._collected_samples = 0
+
+                self.metric.setup(num_samples=self._expected_samples)
+                self._ready_to_accumulate = True
+
+    def on_validation_batch_end(
+        self,
+        trainer: pl.Trainer,
+        pl_module: pl.LightningModule,
+        outputs: Optional[STEP_OUTPUT],
+        batch: Any,
+        batch_idx: int,
+        dataloader_idx: int,
+    ) -> None:
+        if dataloader_idx == self.loader_idx:
+            assert self._ready_to_accumulate
+
+            self.metric.update_data(outputs)
+
+            self._collected_samples += len(outputs[list(outputs.keys())[0]])
+            if self._collected_samples > self._expected_samples:
+                self._raise_computation_error()
+
+            # For non-additive metrics (like f1) we usually accumulate some infortmation during the epoch, then we
+            # calculate final score `on_validation_epoch_end`. The problem here is that `on_validation_epoch_end`
+            # lightning logger doesn't know dataloader_idx and logs metrics in incorrect way if num_dataloaders > 1.
+            # To avoid the problem we calculate metrics `on_validation_batch_end` for the last batch in the loader.
+            is_last_expected_batch = self._collected_samples == self._expected_samples
+            if is_last_expected_batch:
+                self.calc_and_log_metrics(pl_module)
+
+    def on_validation_epoch_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
+        self._ready_to_accumulate = False
+        if self._collected_samples != self._expected_samples:
+            self._raise_computation_error()
+
+    def calc_and_log_metrics(self, pl_module: pl.LightningModule) -> None:
+        metrics = self.metric.compute_metrics()
+        metrics = flatten_dict(metrics, sep="/")
+        pl_module.log_dict(metrics, rank_zero_only=True, add_dataloader_idx=True)
+
+    def _raise_computation_error(self) -> Exception:
+        raise ValueError(
+            f"Incorrect calculation for {self.metric.__class__.__name__} metric. "
+            f"Inconsistent number of samples, obtained: {self._collected_samples}, "
+            f"expected: {self._expected_samples}, "
+            f"'samples_in_getitem': {self.samples_in_getitem}"
+        )
---
+++ oml/utils/images/images_resize.py
@@ -0,0 +1,234 @@
+from functools import partial
+from typing import List, Tuple, Union
+
+import albumentations as albu
+import cv2
+import numpy as np
+import torch
+
+from oml.const import PAD_COLOR
+
+SizeHW = Tuple[int, int]  # Size of images in (height, width) order
+BboxesType = Union[torch.Tensor, np.ndarray]  # in format Nx4: [[left_1, top_1, right_1, bottom_1], ...]
+ImageType = Union[torch.Tensor, np.ndarray]  # ndarray shapes: HxW, HxWxC. tensor shapes : HxW, CxHxW, BxCxHxW
+
+
+def pad_resize(im: np.ndarray, size: int, pad_ratio: float) -> np.ndarray:
+    """
+
+    Args:
+        im: Input image
+        size: Size of the output image
+        pad_ratio: Size of the borders in an output image.
+          Note, borders will appear even for squared images.
+          This value is measured in terms of the part of the original image.
+
+    Returns:
+        Output image with borders
+
+    """
+    assert pad_ratio >= 0
+
+    return albu.Compose(
+        [
+            albu.LongestMaxSize(max_size=int((1 - pad_ratio) * size)),
+            albu.PadIfNeeded(
+                min_height=size,
+                min_width=size,
+                border_mode=cv2.BORDER_CONSTANT,
+                value=PAD_COLOR,
+            ),
+        ]
+    )(image=im)["image"]
+
+
+def karesize_image(image: ImageType, new_hw: SizeHW) -> ImageType:
+    """
+    Function resizes image with keeping aspect ratio
+
+    NOTE: tensors are resized on same device with gradient calculation
+
+    """
+    if isinstance(image, torch.Tensor):
+        is_tensor = True
+    elif isinstance(image, np.ndarray):
+        is_tensor = False
+    else:
+        raise TypeError("Unsupported type")
+
+    src_hw = get_image_hw(image)
+
+    if new_hw == src_hw:
+        return image
+
+    _, resize_hw, padding = get_karesize_param(src_hw, new_hw)
+
+    if is_tensor:
+        image = resize_tensor(image, resize_hw)
+        image = torch.nn.functional.pad(image, padding, mode="constant")
+    else:
+        pad_left, pad_right, pad_top, pad_bottom = padding
+        padding_np = ((pad_top, pad_bottom), (pad_left, pad_right))
+        if image.ndim == 3:
+            padding_np = (*padding_np, (0, 0))  # type: ignore
+        image = resize_ndarray(image, resize_hw)
+        image = np.pad(image, padding_np)
+
+    return image
+
+
+def inverse_karesize_image(image: ImageType, src_hw: SizeHW) -> ImageType:
+    """
+    Function resizes image back to src size. The function inverse to the 'karesize_image' function
+
+    NOTE: tensors are resized on same device with gradient calculation
+
+    """
+    if isinstance(image, torch.Tensor):
+        is_tensor = True
+    elif isinstance(image, np.ndarray):
+        is_tensor = False
+    else:
+        raise TypeError("Unsupported type")
+
+    curr_hw = get_image_hw(image)
+
+    if curr_hw == src_hw:
+        return image
+
+    _, (resized_h, resized_w), (pad_left, _, pad_top, _) = get_karesize_param(src_hw, curr_hw)
+
+    if is_tensor:
+        image = image[..., pad_top : pad_top + resized_h, pad_left : pad_left + resized_w]
+        image = resize_tensor(image, src_hw)
+    else:
+        image = image[pad_top : pad_top + resized_h, pad_left : pad_left + resized_w, ...]
+        image = resize_ndarray(image, src_hw)
+
+    return image
+
+
+def karesize_bboxes(src_bboxes: BboxesType, src_hw: SizeHW, new_hw: SizeHW) -> BboxesType:
+    """
+    Function resizes bboxes with keeping aspect ratio from src size to desired size
+
+    NOTE: tensors are resized on same device with gradient calculation
+
+    """
+    _assert_bbox_type_and_shape(src_bboxes)
+    scale, _, (pad_left, _, pad_top, _) = get_karesize_param(src_hw, new_hw)
+
+    bboxes = src_bboxes * scale
+    bboxes[:, 0] += pad_left
+    bboxes[:, 2] += pad_left
+    bboxes[:, 1] += pad_top
+    bboxes[:, 3] += pad_top
+
+    return bboxes
+
+
+def inverse_karesize_bboxes(cur_bboxes: BboxesType, curr_hw: SizeHW, src_hw: SizeHW) -> BboxesType:
+    """
+    Function resizes bboxes back to src size. The function inverse to the 'karesize_bboxes' function
+
+    NOTE: tensors are resized on same device with gradient calculation
+
+    """
+    _assert_bbox_type_and_shape(cur_bboxes)
+    scale, _, (pad_left, _, pad_top, _) = get_karesize_param(src_hw, curr_hw)
+
+    padding = [[pad_left, pad_top, pad_left, pad_top]]
+
+    if isinstance(cur_bboxes, np.ndarray):
+        constructor = np.array
+    elif isinstance(cur_bboxes, torch.Tensor):
+        constructor = partial(torch.tensor, device=cur_bboxes.device)
+    else:
+        raise TypeError("Unsuported type of bboxes")
+
+    padding = constructor(padding)
+
+    src_bboxes = (cur_bboxes - padding) / scale
+
+    return src_bboxes
+
+
+def _assert_bbox_type_and_shape(cur_bboxes: BboxesType) -> None:
+    assert isinstance(cur_bboxes, (torch.Tensor, np.ndarray))
+    assert cur_bboxes.ndim == 2
+    assert cur_bboxes.shape[1] == 4
+
+
+def get_image_hw(image: Union[np.ndarray, torch.Tensor]) -> SizeHW:
+    if isinstance(image, np.ndarray):
+        image_hw = (image.shape[0], image.shape[1])
+    elif isinstance(image, torch.Tensor):
+        image_hw = (image.shape[-2], image.shape[-1])
+    else:
+        raise TypeError("Only array and tensor types are available")
+    return image_hw
+
+
+def get_karesize_param(src_hw: SizeHW, new_hw: SizeHW) -> Tuple[float, SizeHW, List[int]]:
+    scale = min(new_hw[0] / src_hw[0], new_hw[1] / src_hw[1])
+    resized_hw = (int(scale * src_hw[0]), int(scale * src_hw[1]))
+
+    pad_left = (new_hw[1] - resized_hw[1]) // 2
+    pad_right = new_hw[1] - resized_hw[1] - pad_left
+
+    pad_top = (new_hw[0] - resized_hw[0]) // 2
+    pad_bottom = new_hw[0] - resized_hw[0] - pad_top
+
+    return scale, resized_hw, [pad_left, pad_right, pad_top, pad_bottom]
+
+
+def resize_tensor(tensor: torch.Tensor, new_size_hw: SizeHW) -> torch.Tensor:
+    assert isinstance(tensor, torch.Tensor)
+    ndim = tensor.ndim
+    assert 2 <= ndim <= 4
+
+    dtype = tensor.dtype
+
+    # Tensor must have BxCxHxW shape to interpolate its in spatial dimension
+    for _ in range(4 - ndim):
+        tensor = tensor.unsqueeze(0)
+
+    min_value = torch.min(tensor).item()
+    max_value = torch.max(tensor).item()
+
+    tensor_resized = torch.nn.functional.interpolate(
+        tensor.float(), (new_size_hw[0], new_size_hw[1]), align_corners=True, mode="bicubic"
+    ).clamp(min_value, max_value)
+    for _ in range(4 - ndim):
+        tensor_resized = tensor_resized.squeeze(0)
+
+    if dtype == torch.bool:
+        if tensor.requires_grad:
+            print(
+                "Resizing for torch.bool is not implemented in pytorch. " "Gradients will be discarded due to resizing",
+                flush=True,
+            )
+        thr = (max_value - min_value) / 2
+        tensor_resized = tensor_resized >= thr
+
+    tensor_resized = tensor_resized.to(dtype=dtype)
+
+    return tensor_resized
+
+
+def resize_ndarray(array: np.ndarray, new_size_hw: SizeHW) -> np.ndarray:
+    assert isinstance(array, np.ndarray)
+    ndim = array.ndim
+    assert 2 <= ndim <= 3
+
+    dtype = array.dtype
+
+    if dtype == bool:
+        array = array.astype(np.uint8)
+
+    array_resized = cv2.resize(array, (new_size_hw[1], new_size_hw[0]), interpolation=cv2.INTER_CUBIC).astype(dtype)
+
+    if array_resized.ndim < ndim:
+        array_resized = array_resized[..., np.newaxis]
+
+    return array_resized
