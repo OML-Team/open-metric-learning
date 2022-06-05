b2d3301dcf9de55315c5aa885354bfe5f2fbb11d
DaloroAT <aleksei.v.tarasov@gmail.com>
2022-06-05 16:34:18 +0200

    workflows and workers check

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,16 @@
+from platform import platform
+
+import pytest
+
 from oml.utils.misc import set_global_seed
 
 set_global_seed(42)
+
+
+@pytest.fixture(scope="session")
+def num_workers() -> int:
+    os_platform = platform().lower()
+    if "linux" in os_platform and "microsoft" not in os_platform:
+        return 2
+    else:
+        return 0
