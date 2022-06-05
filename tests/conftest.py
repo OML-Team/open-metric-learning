7bf0d16fcb2f82a7b23d8dc701efa40c6cca6ca1
Aleksei Tarasov <aleksei.v.tarasov@gmail.com>
2022-06-05 22:44:35 +0300

    CI (#5)
    - platform dependent num_workers for tests
    - pre-commit hooks as CI in github actions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,24 @@
+from platform import platform
+
+import pytest
+
 from oml.utils.misc import set_global_seed
 
 set_global_seed(42)
+
+
+@pytest.fixture(scope="session")
+def num_workers() -> int:
+    """
+    Multiprocessing slowly creates new processes with `spawn` start method. Only this method is available on Windows
+    and MacOS systems. To increase performance we use num_workers > 0 only on a pure Linux system.
+
+    We check that `microsoft`, `mac`, etc. are not included in the platform, because the Linux process can be
+    launched from docker or WSL on these host platforms.
+    """
+    os_platform = platform()
+    platforms_with_spawn = ["microsoft", "mac", "osx"]
+    if "linux" in os_platform and any(x not in os_platform for x in platforms_with_spawn):
+        return 2
+    else:
+        return 0
