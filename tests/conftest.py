4c0968f5f7b7d70f25634f8ddefb0023788779a0
DaloroAT <aleksei.v.tarasov@gmail.com>
2022-06-05 17:08:33 +0200

    num_workers platform dependent

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,11 +10,11 @@ set_global_seed(42)
 @pytest.fixture(scope="session")
 def num_workers() -> int:
     """
-    Multiprocessing slowly create new processes with `spawn` start method. Only this method is available on Windows
-    and MacOS systems. To increase performance we use num_workers > 0 only on pure Linux system.
+    Multiprocessing slowly creates new processes with `spawn` start method. Only this method is available on Windows
+    and MacOS systems. To increase performance we use num_workers > 0 only on a pure Linux system.
 
-    We check that `microsoft`, `mac`, etc. are not included into platform, because linux process can be
-    launched from docker or WSL on this host platforms.
+    We check that `microsoft`, `mac`, etc. are not included in the platform, because the Linux process can be
+    launched from docker or WSL on these host platforms.
     """
     os_platform = platform()
     platforms_with_spawn = ['microsoft', 'mac', 'osx']
