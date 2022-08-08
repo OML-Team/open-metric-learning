eca7d9727c4001cdcaa037f40c22212bf51dc248
Aleksei Shabanov <lexibender@ya.ru>
2022-08-08 15:30:33 +0200

    upd

--- tests/test_examples/test_runs_via_configs/val_mock.py
+++ tests/test_examples/test_runs_via_configs/val_mock.py
@@ -5,12 +5,14 @@ from omegaconf import DictConfig
 
 from oml.const import MOCK_DATASET_PATH
 from oml.lightning.entrypoints.validate import pl_val
+from oml.utils.misc import dictconfig_to_dict
 
 
 @hydra.main(config_path="configs", config_name="val_mock.yaml")
 def main_hydra(cfg: DictConfig) -> None:
-    cfg.dataset_root = MOCK_DATASET_PATH
-    cfg.logs_root = tempfile.gettempdir()
+    cfg = dictconfig_to_dict(cfg)
+    cfg["dataset_root"] = MOCK_DATASET_PATH
+    cfg["logs_root"] = tempfile.gettempdir()
     pl_val(cfg)
 
 
