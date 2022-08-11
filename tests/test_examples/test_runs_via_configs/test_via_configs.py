b612c104485e53e5c7c932b34ab4e525b59ebf36
Aleksei Shabanov <lexibender@ya.ru>
2022-08-11 17:58:32 +0200

    upd

--- tests/test_examples/test_runs_via_configs/test_via_configs.py
+++ tests/test_examples/test_runs_via_configs/test_via_configs.py
@@ -1,7 +1,10 @@
+import shutil
 import subprocess
 import warnings
+from pathlib import Path
 
 import pytest
+import yaml  # type: ignore
 
 from oml.const import PROJECT_ROOT
 
@@ -10,7 +13,16 @@ warnings.filterwarnings("ignore")
 SCRIPTS_PATH = PROJECT_ROOT / "tests/test_examples/test_runs_via_configs/"
 
 
+def rm_logs(cfg_name: Path) -> None:
+    with open(SCRIPTS_PATH / "configs" / cfg_name, "r") as f:
+        cfg = yaml.safe_load(f)
+
+    if ("logs_root" in cfg) and Path(cfg["logs_root"]).exists():
+        shutil.rmtree(cfg["logs_root"])
+
+
 @pytest.mark.parametrize("file", ["train_mock.py", "val_mock.py"])
 def test_mock_examples(file: str) -> None:
-    file = SCRIPTS_PATH / file
-    subprocess.run(["python", str(file)], check=True)
+    subprocess.run(["python", str(SCRIPTS_PATH / file)], check=True)
+
+    rm_logs(cfg_name=SCRIPTS_PATH / "configs" / file.replace(".py", ".yaml"))
