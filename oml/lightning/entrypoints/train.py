067d0f591d728531eb561ce053b81290ef60e084
Aleksei Shabanov <shabanoff.aleksei@gmail.com>
2022-08-16 14:24:03 +0200

    Some repo polishing
    added amp to config
    changed docker hub
    added version
    rm logs after tests
    small refactoring for IPostProcessor

--- oml/lightning/entrypoints/train.py
+++ oml/lightning/entrypoints/train.py
@@ -140,9 +140,10 @@ def pl_train(cfg: TCfg) -> None:
         enable_model_summary=True,
         num_nodes=1,
         gpus=cfg["gpus"],
-        strategy=DDPPlugin(find_unused_parameters=False) if cfg["gpus"] else None,
+        strategy=DDPPlugin(find_unused_parameters=False) if (cfg["gpus"] and len(cfg["gpus"]) > 1) else None,
         callbacks=[metrics_clb, ckpt_clb],
         logger=logger,
+        precision=cfg.get("precision", 32),
     )
 
     pl_model = RetrievalModule(model=extractor, criterion=criterion, optimizer=optimizer, scheduler=scheduler)
