55371c56509e5c0ab829fc754992b6d192f9f962
b0nce <kindulov.ml@gmail.com>
2022-08-17 13:14:20 +0000

    [fix] Registry names.

--- oml/registry/losses.py
+++ oml/registry/losses.py
@@ -8,9 +8,9 @@ from oml.registry.miners import get_miner_by_cfg
 from oml.utils.misc import TCfg, dictconfig_to_dict
 
 LOSSES_REGISTRY = {
-    "TripletLoss": TripletLoss,
-    "TripletLossPlain": TripletLossPlain,
-    "TripletLossWithMiner": TripletLossWithMiner,
+    "triplet": TripletLoss,
+    "triplet_plain": TripletLossPlain,
+    "triplet_with_miner": TripletLossWithMiner,
     "CrossEntropyLoss": nn.CrossEntropyLoss,
     "ArcFaceLoss": ArcFaceLoss,
 }
