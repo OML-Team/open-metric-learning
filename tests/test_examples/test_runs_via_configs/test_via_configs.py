ce7c09259a38ce291b4bc0e1d585bda1d578fb98
Aleksei Tarasov <atarasov@newyorker.de>
2022-08-25 14:27:40 +0200

    test for mock tests with ddp fixed

--- tests/test_examples/test_runs_via_configs/test_via_configs.py
+++ tests/test_examples/test_runs_via_configs/test_via_configs.py
@@ -16,16 +16,13 @@ warnings.filterwarnings("ignore")
 SCRIPTS_PATH = PROJECT_ROOT / "tests/test_examples/test_runs_via_configs/"
 
 
-TDevices = Union[int, List[int]]
-
-
-def accelerator_devices_pairs() -> List[Tuple[str, TDevices]]:  # type: ignore
-    pairs = [("cpu", 1), ("cpu", [0, 2])]
+def accelerator_devices_pairs() -> List[Tuple[str, str]]:  # type: ignore
+    pairs = [("cpu", "1"), ("cpu", "[0, 2]")]
 
     if torch.cuda.is_available():
-        pairs += [("gpu", 1)]
+        pairs += [("gpu", "1")]
         if torch.cuda.device_count() > 1:
-            pairs += [("gpu", [0, 1])]
+            pairs += [("gpu", "[0, 1]")]
 
     return pairs  # type: ignore
 
@@ -38,20 +35,18 @@ def rm_logs(cfg_name: Path) -> None:
         shutil.rmtree(cfg["logs_root"])
 
 
-def run(file: str, accelerator: str, devices: TDevices) -> None:
-    cmd = f"python {str(SCRIPTS_PATH / file)} ++accelerator='{accelerator}'"
-    if devices is not None:
-        cmd += f" ++devices='{devices}'"
+def run(file: str, accelerator: str, devices: str) -> None:
+    cmd = f"python {str(SCRIPTS_PATH / file)} ++accelerator='{accelerator}' ++devices='{devices}'"
     subprocess.run(cmd, check=True, shell=True)
 
     rm_logs(cfg_name=SCRIPTS_PATH / "configs" / file.replace(".py", ".yaml"))
 
 
 @pytest.mark.parametrize("accelerator, devices", accelerator_devices_pairs())
-def test_train(accelerator: str, devices: TDevices) -> None:
+def test_train(accelerator: str, devices: str) -> None:
     run("train_mock.py", accelerator, devices)
 
 
 @pytest.mark.parametrize("accelerator, devices", accelerator_devices_pairs())
-def test_val(accelerator: str, devices: TDevices) -> None:
+def test_val(accelerator: str, devices: str) -> None:
     run("val_mock.py", accelerator, devices)
