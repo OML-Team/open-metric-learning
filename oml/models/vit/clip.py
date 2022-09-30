1b8261e563eef28b8a0ff53c9928031caee6c533
b0nce <kindulov.ml@gmail.com>
2022-09-30 12:02:56 +0000

    [fix] Some fixes from PR.

--- oml/models/vit/clip.py
+++ oml/models/vit/clip.py
@@ -8,33 +8,39 @@ from oml.models.utils import TStateDict, filter_state_dict, patch_device_and_flo
 from oml.models.vit.vision_transformer_clip import VisionTransformer
 from oml.utils.io import download_checkpoint
 
-SBER_MODELS_URL = "https://huggingface.co/sberbank-ai"
-OPENAI_MODELS_URL = "https://openaipublic.azureedge.net/clip/models"
-
 
 class ViTCLIPExtractor(IExtractor):
     pretrained_models: Dict[str, Any] = {
         # checkpoints pretrained by OpenAI
         "openai_vitb16_224": (
-            f"{OPENAI_MODELS_URL}/5806e77cd80f8b59890b7e101eabd078d9fb84e6937f9e85e4ecb61988df416f/ViT-B-16.pt",
-            "44c3d8",
+            "https://openaipublic.azureedge.net/clip/models/5806e77cd80f8b59890b7e101eabd078d9fb84e6937f9e85e4ecb61988df416f/ViT-B-16.pt",
+            "44c3d804ecac03d9545ac1a3adbca3a6",
         ),
         "openai_vitb32_224": (
-            f"{OPENAI_MODELS_URL}/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt",
-            "3ba34e",
+            "https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt",
+            "3ba34e387b24dfe590eeb1ae6a8a122b",
         ),
         "openai_vitl14_224": (
-            f"{OPENAI_MODELS_URL}/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt",
-            "096db1",
+            "https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt",
+            "096db1af569b284eb76b3881534822d9",
         ),
         "openai_vitl14_336": (
-            f"{OPENAI_MODELS_URL}/3035c92b350959924f9f00213499208652fc7ea050643e8b385c2dac08641f02/ViT-L-14-336px.pt",
-            "b31105",
+            "https://openaipublic.azureedge.net/clip/models/3035c92b350959924f9f00213499208652fc7ea050643e8b385c2dac08641f02/ViT-L-14-336px.pt",
+            "b311058cae50cb10fbfa2a44231c9473",
         ),
         # checkpoints pretrained by SberbankAI
-        "sber_vitb16_224": (f"{SBER_MODELS_URL}/ruclip-vit-base-patch16-224/resolve/main/pytorch_model.bin", "7882e0"),
-        "sber_vitb32_224": (f"{SBER_MODELS_URL}/ruclip-vit-base-patch32-224/resolve/main/pytorch_model.bin", "e2c4da"),
-        "sber_vitl14_224": (f"{SBER_MODELS_URL}/ruclip-vit-large-patch14-224/resolve/main/pytorch_model.bin", "9b4a1c"),
+        "sber_vitb16_224": (
+            "https://huggingface.co/sberbank-ai/ruclip-vit-base-patch16-224/resolve/main/pytorch_model.bin",
+            "7882e07674d78c674e33cb892a68bbfc",
+        ),
+        "sber_vitb32_224": (
+            "https://huggingface.co/sberbank-ai/ruclip-vit-base-patch32-224/resolve/main/pytorch_model.bin",
+            "e2c4dab46a3cfa608bdd762973e90d32",
+        ),
+        "sber_vitl14_224": (
+            "https://huggingface.co/sberbank-ai/ruclip-vit-large-patch14-224/resolve/main/pytorch_model.bin",
+            "9b4a1cd25d15bad4ffd2ba6e34b8a67c",
+        ),
     }
     jitted_weights = {"openai_vitb16_224", "openai_vitb32_224", "openai_vitl14_224", "openai_vitl14_336"}
 
@@ -157,3 +163,6 @@ def get_vit_config_by_name(model_name: str) -> Dict[str, Any]:
 
     assert model_name in CLIP_MODELS, f"Model {model_name} is unknown."
     return CLIP_MODELS[model_name]
+
+
+__all__ = ["ViTCLIPExtractor"]
