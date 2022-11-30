33b69b0b11527b056b958af518236eec2f1924d0
Aleksei Shabanov <lexibender@ya.ru>
2022-11-30 16:52:14 +0100

    docs

--- oml/losses/surrogate_precision.py
+++ oml/losses/surrogate_precision.py
@@ -7,20 +7,32 @@ from oml.utils.misc_torch import pairwise_dist
 
 
 class SurrogatePrecision(torch.nn.Module):
+    """
+    This loss is a differentiable approximation of Precision@k metric.
+
+    The loss is described in the following paper under a bit different name:
+    `Recall@k Surrogate Loss with Large Batches and Similarity Mixup`_.
+
+    The idea is that we express the formula for Precision@k using two step functions (aka Heaviside functions).
+    Then we approximate them using two sigmoid functions with temperatures.
+    The smaller temperature the close sigmoid to the step function, but the gradients are sparser,
+    and vice versa. In the original paper `t1 = 1.0` and `t2 = 0.01` have been used.
+
+    .. _Recall@k Surrogate Loss with Large Batches and Similarity Mixup:
+        https://arxiv.org/pdf/2108.11179v2.pdf
+
+    """
+
     criterion_name = "surrogate_precision"
 
     def __init__(self, k: int, temperature1: float = 1.0, temperature2: float = 0.01, reduction: str = "mean"):
         """
-        todo: docs
-
-        https://arxiv.org/pdf/2108.11179v2.pdf
-        Recall@k Surrogate Loss with Large Batches and Similarity Mixup
 
         Args:
-            k:
-            temperature1:
-            temperature2:
-            reduction:
+            k: Parameter
+            temperature1: Scaling factor for the 1st sigmoid, see docs above.
+            temperature2: Scaling factor for the 2nd sigmoid, see docs above.
+            reduction: ``mean``, ``sum`` or ``none``
 
         """
         super(SurrogatePrecision, self).__init__()
@@ -30,14 +42,24 @@ class SurrogatePrecision(torch.nn.Module):
         self.temperature2 = temperature2
         self.reduction = reduction
 
-    def forward(self, emb: torch.Tensor, labels: Tensor) -> Tensor:
-        assert len(emb) == len(labels)
+    def forward(self, features: torch.Tensor, labels: Tensor) -> Tensor:
+        """
 
-        bs = len(emb)
-        is_query = torch.ones(bs).bool().to(emb.device)
-        is_gallery = torch.ones(bs).bool().to(emb.device)
+        Args:
+            features: Features with the shape of ``[batch_size, feature_size]``
+            labels: Labels with the size of ``batch_size``
 
-        distances = pairwise_dist(x1=emb[is_query], x2=emb[is_gallery])
+        Returns:
+            Loss value
+
+        """
+        assert len(features) == len(labels)
+
+        bs = len(features)
+        is_query = torch.ones(bs).bool().to(features.device)
+        is_gallery = torch.ones(bs).bool().to(features.device)
+
+        distances = pairwise_dist(x1=features[is_query], x2=features[is_gallery])
         mask_gt = calc_gt_mask(is_query=is_query, is_gallery=is_gallery, labels=labels)
 
         loss = 1 - surrogate_precision(
