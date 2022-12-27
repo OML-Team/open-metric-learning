c80494fc024ce32778195757579cd2b29b163476
Aleksei Shabanov <lexibender@ya.ru>
2022-12-27 11:54:15 +0100

    upd

--- examples/inshop/inshop_experiments.sh
+++ examples/inshop/inshop_experiments.sh
@@ -1,73 +1,35 @@
 ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
 ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=75 sampler.args.n_instances=4 sampler.args.n_categories=2 valid_period=2
+python train_inshop.py criterion.args.margin=0.1
 
 ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
 ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=50 sampler.args.n_instances=4 sampler.args.n_categories=3 valid_period=2
+python train_inshop.py criterion.args.margin=0.12
 
 ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
 ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=37 sampler.args.n_instances=4 sampler.args.n_categories=4 valid_period=2
+python train_inshop.py criterion.args.margin=0.14
 
 ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
 ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=30 sampler.args.n_instances=4 sampler.args.n_categories=5
+python train_inshop.py criterion.args.margin=0.16
 
 ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
 ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=25 sampler.args.n_instances=4 sampler.args.n_categories=6
+python train_inshop.py criterion.args.margin=0.18
 
 ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
 ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=21 sampler.args.n_instances=4 sampler.args.n_categories=7
+python train_inshop.py criterion.args.margin=0.20
 
 ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
 ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=18 sampler.args.n_instances=4 sampler.args.n_categories=8
+python train_inshop.py criterion.args.margin=0.22
 
 ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
 ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=16 sampler.args.n_instances=4 sampler.args.n_categories=9
+python train_inshop.py criterion.args.margin=0.25
 
 ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
 ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=15 sampler.args.n_instances=4 sampler.args.n_categories=10
-
-# the same, but margin
-
-ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
-ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=75 sampler.args.n_instances=4 sampler.args.n_categories=2 model.args.normalise_features=True criterion.args.margin=0.15 valid_period=2
-
-ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
-ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=50 sampler.args.n_instances=4 sampler.args.n_categories=3 model.args.normalise_features=True criterion.args.margin=0.15 valid_period=2
-
-ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
-ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=37 sampler.args.n_instances=4 sampler.args.n_categories=4 model.args.normalise_features=True criterion.args.margin=0.15 valid_period=2
-
-ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
-ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=30 sampler.args.n_instances=4 sampler.args.n_categories=5 model.args.normalise_features=True criterion.args.margin=0.15
-
-ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
-ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=25 sampler.args.n_instances=4 sampler.args.n_categories=6 model.args.normalise_features=True criterion.args.margin=0.15
-
-ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
-ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=21 sampler.args.n_instances=4 sampler.args.n_categories=7 model.args.normalise_features=True criterion.args.margin=0.15
-
-ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
-ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=18 sampler.args.n_instances=4 sampler.args.n_categories=8 model.args.normalise_features=True criterion.args.margin=0.15
-
-ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
-ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=16 sampler.args.n_instances=4 sampler.args.n_categories=9 model.args.normalise_features=True criterion.args.margin=0.15
-
-ps aux | grep train_inshop.py | awk '{print $2}' | xargs kill -9 $1
-ps aux | grep multiprocessing.spawn | awk '{print $2}' | xargs kill -9 $1
-python train_inshop.py sampler.args.n_labels=15 sampler.args.n_instances=4 sampler.args.n_categories=10 model.args.normalise_features=True criterion.args.margin=0.15
+python train_inshop.py criterion.args.margin=0.30
