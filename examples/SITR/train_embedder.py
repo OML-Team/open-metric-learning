8bd334898a8447e2540806207ebc156930c3d438
Aleksei Shabanov <lexibender@ya.ru>
2023-02-16 16:13:52 +0100

    upd

---
+++ examples/SITR/train_embedder.py
@@ -0,0 +1,13 @@
+import hydra
+from omegaconf import DictConfig
+
+from oml.lightning.entrypoints.train import pl_train
+
+
+@hydra.main(config_path=".", config_name="train_embedder.yaml")
+def main_hydra(cfg: DictConfig) -> None:
+    pl_train(cfg)
+
+
+if __name__ == "__main__":
+    main_hydra()
