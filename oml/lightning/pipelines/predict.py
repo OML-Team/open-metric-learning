8dc04a035d797c1c139a9caabec42bda98e52ae2
Aleksei Shabanov <shabanoff.aleksei@gmail.com>
2023-06-07 16:15:43 +0200

    Added prediction pipeline
    Added prediction pipeline

---
+++ oml/lightning/pipelines/predict.py
@@ -0,0 +1,76 @@
+import itertools
+import json
+from pathlib import Path
+from pprint import pprint
+
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+
+from oml.const import IMAGE_EXTENSIONS, TCfg
+from oml.datasets.list_dataset import ListDataset
+from oml.ddp.utils import get_world_size_safe, is_main_process, sync_dicts_ddp
+from oml.lightning.modules.extractor import ExtractorModule
+from oml.lightning.pipelines.parser import parse_engine_params_from_config
+from oml.registry.models import get_extractor_by_cfg
+from oml.registry.transforms import get_transforms_by_cfg
+from oml.transforms.images.utils import get_im_reader_for_transforms
+from oml.utils.images.images import find_broken_images
+from oml.utils.misc import dictconfig_to_dict
+
+
+def extractor_prediction_pipeline(cfg: TCfg) -> None:
+    """
+    This pipeline allows you to save features extracted by a feature extractor.
+
+    The config can be specified as a dictionary or with hydra: https://hydra.cc/.
+    For more details look at ``pipelines/features_extraction/README.md``
+
+    """
+    cfg = dictconfig_to_dict(cfg)
+
+    pprint(cfg)
+
+    transforms = get_transforms_by_cfg(cfg["transforms_predict"])
+    filenames = [list(Path(cfg["data_dir"]).glob(f"**/*.{ext}")) for ext in IMAGE_EXTENSIONS]
+    filenames = list(itertools.chain(*filenames))
+
+    f_imread = get_im_reader_for_transforms(transforms)
+
+    print("Let's check if there are broken images:")
+    broken_images = find_broken_images(filenames, f_imread=f_imread)
+    if broken_images:
+        raise ValueError(f"There are images that cannot be open:\n {broken_images}.")
+
+    dataset = ListDataset(filenames_list=filenames, transform=transforms, f_imread=f_imread)
+
+    loader = DataLoader(
+        dataset=dataset, batch_size=cfg["bs"], num_workers=cfg["num_workers"], shuffle=False, drop_last=False
+    )
+
+    extractor = get_extractor_by_cfg(cfg["extractor"])
+    pl_model = ExtractorModule(extractor=extractor)
+
+    trainer_engine_params = parse_engine_params_from_config(cfg)
+    trainer_engine_params["replace_sampler_ddp"] = True
+    trainer = pl.Trainer(precision=cfg.get("precision", 32), **trainer_engine_params)
+    predictions = trainer.predict(model=pl_model, dataloaders=loader, return_predictions=True)
+
+    paths, embeddings = [], []
+    for prediction in predictions:
+        paths.extend(prediction[dataset.paths_key])
+        embeddings.extend(prediction[pl_model.embeddings_key].tolist())
+
+    paths = sync_dicts_ddp({"key": list(map(str, paths))}, get_world_size_safe())["key"]
+    embeddings = sync_dicts_ddp({"key": embeddings}, get_world_size_safe())["key"]
+
+    if is_main_process():
+        data_to_save = dict(zip(paths, embeddings))
+        save_path = Path(cfg["save_dir"]) / "predictions.json"
+
+        with open(save_path, "w") as f:
+            json.dump(data_to_save, f)
+
+        print(f"{len(paths)} predictions have been saved to {save_path}")
+
+
+__all__ = ["extractor_prediction_pipeline"]
--- pipelines/features_extraction/extractor_inshop/README.md
+++ pipelines/features_extraction/extractor_inshop/README.md
@@ -4,5 +4,6 @@
 `python ../datasets_converters/convert_inshop.py --dataset_root=data/DeepFashion_InShop --no_bboxes`
 2. Command to train: `python train_inshop.py`
 3. Command to validate: `python val_inshop.py`
+4. Command to inference: `python predict_inshop.py`
 
 The weights of a trained model may be found in the [Models Zoo](https://github.com/OML-Team/open-metric-learning#zoo).
