a1a5d3e74baf15896b573ce47fd27d1a4b9924b4
Aleksei Shabanov <shabanoff.aleksei@gmail.com>
2023-08-08 20:34:45 +0200

    Better error message for wandb & neptune loggers
    Better error message for wandb & neptune loggers

--- oml/registry/loggers.py
+++ oml/registry/loggers.py
@@ -1,3 +1,4 @@
+import os
 from typing import Any, Dict, Union
 
 from pytorch_lightning.loggers import (
@@ -12,8 +13,19 @@ from oml.utils.misc import dictconfig_to_dict
 
 LOGGERS_REGISTRY = {"wandb": WandbLogger, "neptune": NeptuneLogger, "tensorboard": TensorBoardLogger}
 
+CLOUD_TOKEN_NAMES = {"wandb": "WANDB_API_KEY", "neptune": "NEPTUNE_API_TOKEN"}
+TOKEN_ERROR_MESSAGE = (
+    "{} logger is specified in your config file, "
+    "but <{}> is not provided as a global environment variable."
+    "Please, execute `export {}=your_token` before running the script."
+)
+
 
 def get_logger(name: str, **kwargs: Dict[str, Any]) -> LightningLoggerBase:
+    if (name in CLOUD_TOKEN_NAMES) and (CLOUD_TOKEN_NAMES[name] not in os.environ):
+        token_name = CLOUD_TOKEN_NAMES[name]
+        raise ValueError(TOKEN_ERROR_MESSAGE.format(name.upper(), token_name, token_name))
+
     return LOGGERS_REGISTRY[name](**kwargs)
 
 
