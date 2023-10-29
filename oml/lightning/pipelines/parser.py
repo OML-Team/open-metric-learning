984a3db3123139d2c8b73e8f97f08c7acf97e66e
alekseysh <lexibender@ya.ry>
2023-10-29 16:26:52 +0100

    find_unused_parameters as arg

--- oml/lightning/pipelines/parser.py
+++ oml/lightning/pipelines/parser.py
@@ -44,7 +44,7 @@ def parse_engine_params_from_config(cfg: TCfg) -> Dict[str, Any]:
         devices = len(devices)
 
     if (isinstance(devices, int) and devices > 1) or (isinstance(devices, (list, tuple)) and len(devices) > 1):
-        strategy = DDPStrategy(find_unused_parameters=True)
+        strategy = DDPStrategy(find_unused_parameters=cfg.get("find_unused_parameters", False))
     else:
         strategy = "auto"
 
