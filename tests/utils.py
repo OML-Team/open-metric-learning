90254dcdfca81895d2b100481d6ac3532b381068
alekseysh <shabanoff.aleksei@gmail.com>
2024-05-20 23:56:30 +0600

    upd

--- tests/utils.py
+++ tests/utils.py
@@ -7,15 +7,15 @@ from torch import FloatTensor, LongTensor, Tensor
 def check_if_sequence_of_tensors_are_equal(list1: Sequence[Tensor], list2: Sequence[Tensor]) -> bool:
     for l1, l2 in zip(list1, list2):
 
-        if isinstance(list1, FloatTensor) and isinstance(list2, FloatTensor):
+        if isinstance(l1, FloatTensor) and isinstance(l2, FloatTensor):
             if not torch.allclose(l1, l2):
                 return False
 
-        elif isinstance(list1, LongTensor) and isinstance(list2, LongTensor):
+        elif isinstance(l1, LongTensor) and isinstance(l2, LongTensor):
             if not (l1 == l2).all():
                 return False
 
         else:
-            raise TypeError(f"Unsupported types: {type(list1)}, {type(list2)}.")
+            raise TypeError(f"Unsupported types: {type(l1)}, {type(l2)}.")
 
     return True
