1effc43a51c98fd382cee31255943e782da6adaf
alekseysh <shabanoff.aleksei@gmail.com>
2024-05-30 04:12:28 +0600

    upd_test

--- tests/test_oml/test_models/test_text_models.py
+++ tests/test_oml/test_models/test_text_models.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 import torch
 from torch.utils.data import default_collate
@@ -10,15 +8,15 @@ from oml.utils import get_mock_texts_dataset
 
 
 @pytest.mark.needs_optional_dependency
-@pytest.mark.skipif(os.getenv("DOWNLOAD_ZOO_IN_TESTS") != "yes", reason="It's a traffic consuming test.")
 @pytest.mark.parametrize(
     "model_name, feat_dim", [("bert-base-uncased", 768), ("roberta-base", 768), ("distilbert-base-uncased", 768)]
 )
 def test_padding_doesnt_affect_outputs(model_name: str, feat_dim: int) -> None:
-    from transformers import AutoModel, AutoTokenizer
+    from transformers import AutoConfig, AutoModel, AutoTokenizer
 
     df_train, _ = get_mock_texts_dataset()
-    extractor = HFWrapper(AutoModel.from_pretrained(model_name), feat_dim).eval()
+    model_cfg = AutoConfig.from_pretrained(model_name)
+    extractor = HFWrapper(AutoModel.from_config(model_cfg), feat_dim).eval()
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     train1 = TextLabeledDataset(df_train, tokenizer=tokenizer, max_length=200)
