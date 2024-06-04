92205469e4c2c57f198d58237dfb299c1ea65dca
alekseysh <shabanoff.aleksei@gmail.com>
2024-06-04 22:14:25 +0600

    upd

--- oml/retrieval/postprocessors/thresholding.py
+++ oml/retrieval/postprocessors/thresholding.py
@@ -1,5 +1,37 @@
+from copy import deepcopy
+
 from oml.interfaces.retrieval import IRetrievalPostprocessor
 from oml.retrieval import RetrievalResults
+
+
+class ConstantThresholding(IRetrievalPostprocessor):
+    def __init__(self, th: float):
+        self.th = th
+
+    def process(self, rr: RetrievalResults) -> RetrievalResults:  # type: ignore
+        """
+        Args:
+            rr: An instance of `RetrievalResults`.
+
+        Returns:
+            An updated instance of `RetrievalResults` where retrieved items having
+            distance greater than `self.th` have been filtered out.
+
+        """
+        distances_upd = []
+        retrieved_ids_upd = []
+
+        for dists, rids in zip(rr.distances, rr.retrieved_ids):
+            mask = dists < self.th
+            dists = dists[mask]
+            rids = rids[mask]
+
+            distances_upd.append(dists)
+            retrieved_ids_upd.append(rids)
+
+        rr = RetrievalResults(distances=distances_upd, retrieved_ids=retrieved_ids_upd, gt_ids=deepcopy(rr.gt_ids))
+
+        return rr
 
 
 class AdaptiveThresholding(IRetrievalPostprocessor):
@@ -10,4 +42,4 @@ class AdaptiveThresholding(IRetrievalPostprocessor):
         return rr
 
 
-__all__ = ["AdaptiveThresholding"]
+__all__ = ["AdaptiveThresholding", "ConstantThresholding"]
--- tests/test_oml/test_postprocessor/test_thresholding.py
+++ tests/test_oml/test_postprocessor/test_thresholding.py
@@ -1,6 +1,49 @@
-from oml.retrieval.postprocessors.thresholding import AdaptiveThresholding
+from torch import FloatTensor, LongTensor
+
+from oml.retrieval.postprocessors.thresholding import (
+    AdaptiveThresholding,
+    ConstantThresholding,
+)
+from oml.retrieval.retrieval_results import RetrievalResults
+from tests.utils import check_if_sequence_of_tensors_are_equal
 
 
 def test_adaptive_thresholding() -> None:
     AdaptiveThresholding(th=0.5)
     assert True
+
+
+def test_thresholding() -> None:
+    th = 0.2
+
+    rr = RetrievalResults(
+        distances=[
+            FloatTensor([0.0, 0.05, 0.07, 0.1, 0.12, 0.2, 0.2, 0.4]),
+            FloatTensor([]),
+            FloatTensor([0.21, 0.25]),
+        ],
+        retrieved_ids=[LongTensor([0, 1, 2, 3, 4, 5, 6, 7]), LongTensor([]), LongTensor([3, 5])],
+        gt_ids=[
+            LongTensor([0, 1]),
+            LongTensor([1, 500]),
+            LongTensor([10, 20]),
+        ],
+    )
+
+    rr_upd_expected = RetrievalResults(
+        distances=[FloatTensor([0.0, 0.05, 0.07, 0.1, 0.12]), FloatTensor([]), FloatTensor([])],
+        retrieved_ids=[LongTensor([0, 1, 2, 3, 4]), LongTensor([]), LongTensor([])],
+        gt_ids=[
+            LongTensor([0, 1]),
+            LongTensor([1, 500]),
+            LongTensor([10, 20]),
+        ],
+    )
+
+    processor = ConstantThresholding(th=th)
+
+    rr_upd = processor.process(rr)
+
+    assert check_if_sequence_of_tensors_are_equal(rr_upd_expected.distances, rr_upd.distances)
+    assert check_if_sequence_of_tensors_are_equal(rr_upd_expected.retrieved_ids, rr_upd.retrieved_ids)
+    assert check_if_sequence_of_tensors_are_equal(rr_upd_expected.gt_ids, rr_upd.gt_ids)
