0334705affd486faec6a9d0e06a6f4993c5e0c66
Aleksei Shabanov <shabanoff.aleksei@gmail.com>
2024-06-10 02:43:59 +0600

    Supporting separated queries and galleries
    Changelog:
    * Added `batched_knn_qg`. So `batched_knn` is just an adapter for `batched_knn_qg`.
    * Added a few methods to `RetrievalResults`: `from_embeddings_qg`, `visuaze_qg`, `visualize_with_functions`. Also added these methods to docs.
    * Added an example to Readme and docs where new methods are shown.
    * Updated tests: added text modality to `test_retrieval_results`, added new `test_retrieval_results_separated_qg`

--- tests/test_oml/test_retrieval_results/test_retrieval_results.py
+++ tests/test_oml/test_retrieval_results/test_retrieval_results.py
@@ -1,12 +1,17 @@
-from pathlib import Path
-
+import matplotlib
 import matplotlib.pyplot as plt
 import pytest
 import torch
 from torch import FloatTensor, LongTensor, nn
 
-from oml.const import LABELS_COLUMN, MOCK_DATASET_PATH, PATHS_COLUMN
+from oml.const import LABELS_COLUMN
+from oml.datasets import (
+    TextBaseDataset,
+    TextQueryGalleryDataset,
+    TextQueryGalleryLabeledDataset,
+)
 from oml.datasets.images import (
+    ImageBaseDataset,
     ImageQueryGalleryDataset,
     ImageQueryGalleryLabeledDataset,
 )
@@ -14,11 +19,16 @@ from oml.inference import inference
 from oml.interfaces.datasets import IVisualizableDataset
 from oml.models import ResnetExtractor
 from oml.retrieval.retrieval_results import RetrievalResults
-from oml.utils.download_mock_dataset import download_mock_dataset
+from oml.utils.download_mock_dataset import (
+    download_mock_dataset,
+    get_mock_texts_dataset,
+)
+from oml.utils.misc_torch import is_sorted_tensor
 from tests.test_integrations.utils import (
     EmbeddingsQueryGalleryDataset,
     EmbeddingsQueryGalleryLabeledDataset,
 )
+from tests.utils import DummyNLPModel
 
 
 def get_model_and_datasets_images(with_gt_labels):  # type: ignore
@@ -26,7 +36,6 @@ def get_model_and_datasets_images(with_gt_labels):  # type: ignore
 
     for df_name in ["df.csv", "df_with_bboxes.csv", "df_with_sequence.csv"]:
         _, df_val = download_mock_dataset(global_paths=True, df_name=df_name)
-        df_val[PATHS_COLUMN] = df_val[PATHS_COLUMN].apply(lambda x: Path(MOCK_DATASET_PATH) / x)
 
         if with_gt_labels:
             dataset = ImageQueryGalleryLabeledDataset(df_val)
@@ -39,6 +48,22 @@ def get_model_and_datasets_images(with_gt_labels):  # type: ignore
     model = ResnetExtractor(weights=None, arch="resnet18", gem_p=None, remove_fc=True, normalise_features=False)
 
     return datasets, model
+
+
+def get_model_and_datasets_texts(with_gt_labels):  # type: ignore
+    from transformers import AutoTokenizer
+
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    model = DummyNLPModel(vocab_size=tokenizer.vocab_size)
+
+    _, df_val = get_mock_texts_dataset()
+    if with_gt_labels:
+        dataset = TextQueryGalleryLabeledDataset(df_val, tokenizer=tokenizer)
+    else:
+        del df_val[LABELS_COLUMN]
+        dataset = TextQueryGalleryDataset(df_val, tokenizer=tokenizer)
+
+    return [dataset], model
 
 
 def get_model_and_datasets_embeddings(with_gt_labels):  # type: ignore
@@ -60,8 +85,18 @@ def get_model_and_datasets_embeddings(with_gt_labels):  # type: ignore
 
 
 @pytest.mark.parametrize("with_gt_labels", [False, True])
-@pytest.mark.parametrize("data_getter", [get_model_and_datasets_embeddings, get_model_and_datasets_images])
-def test_retrieval_results_om_images(with_gt_labels, data_getter) -> None:  # type: ignore
+@pytest.mark.parametrize(
+    "data_getter",
+    [
+        get_model_and_datasets_embeddings,
+        get_model_and_datasets_images,
+        pytest.param(get_model_and_datasets_texts, marks=pytest.mark.needs_optional_dependency),
+    ],
+)
+def test_retrieval_results(with_gt_labels, data_getter) -> None:  # type: ignore
+    current_backend = matplotlib.get_backend()
+    matplotlib.use("Agg")
+
     datasets, model = data_getter(with_gt_labels=with_gt_labels)
 
     for dataset in datasets:
@@ -76,13 +111,14 @@ def test_retrieval_results_om_images(with_gt_labels, data_getter) -> None:  # ty
         assert len(rr.distances) == n_query
 
         for dist in rr.distances:
-            assert (dist[:-1] <= dist[1:]).all()
+            assert is_sorted_tensor(dist)
             assert len(dist) == top_n
 
         if with_gt_labels:
             assert rr.gt_ids is not None
 
         error_expected = not isinstance(dataset, IVisualizableDataset)
+
         if error_expected:
             with pytest.raises(TypeError):
                 fig = rr.visualize(query_ids=[0, 3], dataset=dataset, n_galleries_to_show=3, show=True)
@@ -91,10 +127,14 @@ def test_retrieval_results_om_images(with_gt_labels, data_getter) -> None:  # ty
             fig = rr.visualize(query_ids=[0, 3], dataset=dataset, n_galleries_to_show=3, show=True)
             plt.close(fig=fig)
 
+    matplotlib.use(current_backend)
     assert True
 
 
 def test_visualisation_for_different_number_of_retrieved_items() -> None:
+    current_backend = matplotlib.get_backend()
+    matplotlib.use("Agg")
+
     datasets, _ = get_model_and_datasets_images(with_gt_labels=False)
     # just some random RR with different shapes
     rr = RetrievalResults(
@@ -113,6 +153,8 @@ def test_visualisation_for_different_number_of_retrieved_items() -> None:
     )
     fig = rr.visualize(query_ids=[0, 1, 2, 3], dataset=datasets[0], show=True)
     plt.close(fig=fig)
+
+    matplotlib.use(current_backend)
 
 
 def test_retrieval_results_creation() -> None:
@@ -155,4 +197,40 @@ def test_retrieval_results_creation() -> None:
         gt_ids=[LongTensor([1])],
     )
 
+    assert True
+
+
+@pytest.mark.needs_optional_dependency
+def test_retrieval_results_separated_qg() -> None:
+    from transformers import AutoTokenizer
+
+    current_backend = matplotlib.get_backend()
+    matplotlib.use("Agg")
+
+    # GALLERIES ARE IMAGES
+    _, df_val = download_mock_dataset(global_paths=True, df_name="df.csv")
+    model_g = ResnetExtractor(weights=None, arch="resnet18", gem_p=None, remove_fc=True, normalise_features=False)
+    dataset_g = ImageBaseDataset(paths=df_val["path"].tolist())
+    embeddings_g = inference(model_g, dataset_g, batch_size=2, num_workers=0).float()
+
+    # QUERIES ARE TEXTS AND THEY COME IN BATCHES
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    model_q = DummyNLPModel(vocab_size=tokenizer.vocab_size, emb_size=model_g.feat_dim)
+
+    for texts in [["Cross", "Square"], ["Circle", "Nice cross", "Bad square"]]:
+        dataset_q = TextBaseDataset(texts, tokenizer=tokenizer)
+        embeddings_q = inference(model_q, dataset_q, batch_size=2, num_workers=0).float()
+
+        rr = RetrievalResults.from_embeddings_qg(
+            embeddings_query=embeddings_q,
+            embeddings_gallery=embeddings_g,
+            dataset_query=dataset_q,
+            dataset_gallery=dataset_g,
+            n_items=3,
+        )
+        rr.visualize_qg(query_ids=[0, 1], dataset_query=dataset_q, dataset_gallery=dataset_g, show=True)
+
+        assert rr.gt_ids is None
+
+    matplotlib.use(current_backend)
     assert True
